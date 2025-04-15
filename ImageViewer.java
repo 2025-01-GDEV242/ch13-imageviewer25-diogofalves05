ccecd502df2145422c6e22ffada5a041e1b537c4
Diogo Alves <diogofalves05@gmail.com>
2025-04-15 19:56:24 -0400

    Added GreenChannelFilter class and registered it in ImageViewer

--- ImageViewer.java
+++ ImageViewer.java
@@ -255,7 +255,8 @@ public class ImageViewer
         filterList.add(new GrayScaleFilter("Grayscale"));
         filterList.add(new EdgeFilter("Edge Detection"));
         filterList.add(new FishEyeFilter("Fish Eye"));
-        filterList.add(new RedChannelFilter("Red Channel"));  
+        filterList.add(new RedChannelFilter("Red Channel"));
+        filterList.add(new GreenChannelFilter("Green Channel"));
         return filterList;
     }
     
